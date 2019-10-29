7afa1114fd7938df7a8f73dc1df770ee21d2d481
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-10-29 22:34:13 +0000

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git add . &&
 git commit -m $1 &&
 git push origin master &&
 ssh root@164.132.193.242 <<EOF
-cd aulahaskadsm &&
+cd aulahaskell &&
 git pull origin master &&
 stack build &&
 lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
