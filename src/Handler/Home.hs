fad4aafdf5dbab66f6ba3fc38c62e721f8a2d5b5
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-10-29 22:43:36 +0000

    Deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -21,8 +21,8 @@ getHomeR = do
         toWidgetHead [cassius|
             h1
                 color : blue;
-                width: 100px;
-                background: 000;
+                width: 500px;
+                background-color: #000000;
         |]    
         [whamlet|
             <h1>
