bb61fc53cee280e6a9898ad1c4e171872454c63a
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-05 22:38:56 +0000

    MENU

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git add . &&
-git commit -m $1 &&
+git commit -m "$1" &&
 git push origin master &&
 ssh root@164.132.193.242 <<EOF
 cd aulahaskell &&
--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,6 +9,20 @@ module Handler.Home where
 import Import
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
+
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page2.hamlet")
+
+getPage1R :: Handler Html
+getPage1R = do
+    defaultLayout $ do
+        $(whamletFile "templates/page1.hamlet")
+        toWidgetHead $ (luciusFile "templates/page1.lucius")
+        toWidgetHead $ (juliusFile "templates/page1.lucius")
 
 getHomeR :: Handler Html
 getHomeR = do
