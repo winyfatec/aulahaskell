4d6417778d5fbe4daa6ee4c6b9a7f65474fda9b2
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-05 02:52:48 +0000

    Teste

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -16,7 +16,7 @@ getHomeR = do
     --addStylesheet (StaticR css_bootstrap_css)
     --addScript (StaticR js_main_js)
         setTitle "Aula Haskell"
-        --addScript $ StaticR js_main_js
+        addScript $ StaticR js_main_js
         addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
         toWidgetHead [julius|
             function ola(){
