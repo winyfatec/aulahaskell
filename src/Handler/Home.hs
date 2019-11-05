7196ecf6d73058284c9276b3d4825741dd6daa44
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-05 02:26:17 +0000

    Deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,3 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+--import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
