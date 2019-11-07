2079162b9c2a3e3ae6c9be53da442445e589d4e5
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-07 11:29:38 +0000

    Adsense

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -45,6 +45,13 @@ getHomeR = do
                 color : blue;
         |]    
         [whamlet|
+            <script async src="//pagead2.googlesyndication.com/
+                pagead/js/adsbygoogle.js"></script>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "pub-4764459455736825",
+                enable_page_level_ads: true
+                }); 
             <div class="container">
             
             
