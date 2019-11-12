de7ccc1f6f77920010600155413c60a9d7967f70
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-12 23:10:35 +0000

    Cadastro de Usuario

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeFamilies #-}
-module Handler.Aula where
+module Handler.Usuario where
 
 import Import
 --import Network.HTTP.Types.Status
