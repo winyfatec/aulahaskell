abcd6fdfbe40941b991b85e53e67edd4d4f5c137
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-12 23:34:07 +0000

    Login de Usuario

--- src/Application.hs
+++ src/Application.hs
@@ -41,6 +41,7 @@ import Handler.Home
 import Handler.Aula
 import Handler.Produto
 import Handler.Usuario
+import Handler.Login
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ src/Handler/Login.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Login where
+
+import Import
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
+
+
+formLogin :: Form (Text,Text)
+formLogin  = renderBootstrap $ (,)
+    <$> areq emailField "Email" Nothing
+    <*> areq passwordField "Senha" Nothing
+
+
+
+getLoginR :: Handler Html
+getLoginR = do
+    (widget,enctype) <- generateFormPost formLogin
+    defaultLayout $ do
+        msg <- getMessage
+        [whamlet|
+            $maybe mensa <- msg
+                <div>
+                    ^{mensa}
+            $nothing
+            
+            <h1>
+                Login
+            <form method=post action=@{LoginR}>
+                ^{widget}
+                <input type="submit" value="Entrar">
+        |]
+
+
+postLoginR :: Handler Html
+postLoginR = do
+    ((result,_),_) <- runFormPost formLogin
+    case result of
+        FormSuccess (usuario,senha) -> do
+            usuario <- runDB $ getBy (Unique email)
+            case usuario of
+                Nothing -> do
+                    setMessage[shamlet|
+                        <div>
+                            Email nao encntrado
+                    |]
+                    redirect LoginR
+                Just(_,usr) -> do
+                    if(usuarioSenha usr == senha) then do
+                        setSession "_NOME" (usuarioNome usr)
+                        redirect HomeR
+                    else do
+                        setMessage[shamlet|
+                            <div>
+                                Senha invalida
+                        |]
+                        redirect LoginR
+        _ -> redirect HomeR
+
