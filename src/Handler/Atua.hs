6d1db7ec3547b1a8f3cf4f97d9a55ba0ba007b6a
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-22 00:57:58 +0000

    Fix

--- src/Handler/Atua.hs
+++ src/Handler/Atua.hs
@@ -1,63 +1,62 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Atua where
 
 import Import
---import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
 import Text.Lucius
 import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
 
 serieCB = do
-    rows <- runDB $ selectList [] [Asc SerieNome]
-    optionsPairs $ 
-        map (\r -> (serieNome $ entityVal r, entityKey r)) rows
-        
+  rows <- runDB $ selectList [] [Asc SerieNome]
+  optionsPairs $ 
+      map (\r -> (serieNome $ entityVal r, entityKey r)) rows
 
 atorCB = do
-    rows <- runDB $ selectList [] [Asc AtorNome]
-    optionsPairs $ 
-        map (\r -> (atorNome $ entityVal r, entityKey r)) rows
-        
-formAtua :: Form Atua
-formAtua  = renderBootstrap $ Atua
-    <$> areq (selectField serieCB) "Serie" Nothing
-    <*> areq (selectField atorCB) "Ator" Nothing
-    <*> areq intField "Ano" Nothing
+  rows <- runDB $ selectList [] [Asc AtorNome]
+  optionsPairs $ 
+      map (\r -> (atorNome $ entityVal r, entityKey r)) rows
 
+-- renderDivs
+formAtua :: Form Atua 
+formAtua = renderBootstrap $ Atua
+    <$> areq (selectField serieCB) "Serie: " Nothing
+    <*> areq (selectField atorCB) "Ator: " Nothing
+    <*> areq intField "Ano: " Nothing
 
+    
 getAtuaR :: Handler Html
-getAtuaR = do
-    (widget,enctype) <- generateFormPost formAtua
-    defaultLayout $ do
-        msg <- getMessage
+getAtuaR = do 
+    (widget,_) <- generateFormPost formAtua
+    msg <- getMessage
+    defaultLayout $ 
         [whamlet|
-            $maybe mensa <- msg
+            $maybe mensa <- msg 
                 <div>
                     ^{mensa}
-            $nothing
             
             <h1>
-                Cadastro de Atuacao
+                CADASTRO DE ATUACOES
+            
             <form method=post action=@{AtuaR}>
                 ^{widget}
                 <input type="submit" value="Cadastrar">
         |]
 
-
 postAtuaR :: Handler Html
-postAtuaR = do
+postAtuaR = do 
     ((result,_),_) <- runFormPost formAtua
-    case result of
-        FormSuccess atua -> do
-            runDB $ insert atua
+    case result of 
+        FormSuccess atua -> do 
+            runDB $ insert atua 
             setMessage [shamlet|
-                <h2>
-                    Atuacao inserido com sucesso
+                <div>
+                    ATUACAO INCLUIDA
             |]
             redirect AtuaR
         _ -> redirect HomeR
@@ -79,3 +78,4 @@ getElencoR serieid = do
                     <li>
                         #{atorNome ator}
         |]
+      
