6980551fbd7dbdd61613307e7861864cd2461b2b
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-30 23:50:45 +0000

    Teste P2

---
+++ src/Handler/Chat.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Chat where
+
+import Import
+import Text.Lucius
+import Text.Julius
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql
+
+-- renderDivs
+formChat :: Form Chat 
+formChat = renderBootstrap $ Chat
+    <$> areq textField "Nome: " Nothing
+    <*> areq textField "Mensagem: " Nothing
+
+getChatR :: Handler Html
+getChatR = do 
+    (widget,_) <- generateFormPost formChat
+    msg <- getMessage
+    defaultLayout $ 
+        [whamlet|
+            $maybe mensa <- msg 
+                <div>
+                    ^{mensa}
+            
+            <h1>
+                Chat
+            
+            <form method=post action=@{ChatR}>
+                ^{widget}
+                <input type="submit" value="Enviar">
+        |]
+
+postChatR :: Handler Html
+postChatR = do 
+    ((result,_),_) <- runFormPost formChat
+    case result of 
+        FormSuccess chat -> do 
+            runDB $ insert chat 
+            setMessage [shamlet|
+                <div>
+                    MENSAGEM POSTADA
+            |]
+            redirect ChatR
+        _ -> redirect HomeR
+
+
