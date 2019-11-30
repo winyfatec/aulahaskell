123d226c7255b64e89755ca5d3e475a9ba79ced5
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-11-30 23:46:14 +0000

    Teste P2

--- src/Handler/UserLogin.hs
+++ src/Handler/UserLogin.hs
@@ -47,7 +47,7 @@ postUserLoginR = do
             setSession "_NOME" "root"
             redirect HomeR
         FormSuccess (username,senha) -> do
-            usuario <- runDB $ getBy (Username UniqueUsername)
+            usuario <- runDB $ getBy (UniqueUsername username)
             case usuario of
                 Nothing -> do
                     setMessage[shamlet|
