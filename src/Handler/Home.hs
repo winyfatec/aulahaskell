c88f18a761e0dfddb1c709b33f418690de9cd49c
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 19:52:57 +0000

    Teste P2

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -37,15 +37,7 @@ getHomeR = do
         --addScriptRemote "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async
         addStylesheet $ StaticR css_main_css
         addScript $ StaticR js_main_js
-        toWidgetHead [julius|
-            function ola(){
-                alert("OI");
-            }
-        |]
-        toWidgetHead [cassius|
-            h1
-                color : black;
-        |]    
+        addScript $ StaticR js_pacman_js
         toWidgetHead [hamlet|
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -57,35 +49,12 @@ getHomeR = do
         $(whamletFile "templates/menu.hamlet")
         [whamlet|
         
-            <div class="container">
+            <div .container>
                 $maybe nomeSess <- sess
                     <div> 
-                        Ola #{nomeSess}  
-                        <div>
-                            teste 1
-                        <form action="@{UserLogoutR}" method=post>
-                            <input type="submit" value="Sair">
-                $nothing                 
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src=@{StaticR slide1_jpg}>
-                                <div class="carousel-caption d-none d-md-block">
-                                    <span class="legenda">
-                                        Em breve...
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src=@{StaticR slide2_jpg}>
-                                <div class="carousel-caption d-none d-md-block">
-                                    <span class="legenda">
-                                        Em breve...
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true">
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true">
-                        
-                        
-                <div class="foot">
-                    <img id="imgfatec" src=@{StaticR fatec_png}>
-                    <span class="copytext">
-                        Desenvolvido para a disciplina de Tópicos Especiais em Informática na Fatec BS
+                        Bem-vindo, #{nomeSess}  
+                    <div #pacman>
+                $nothing
+                    
         |]
+        $(whamletFile "templates/footer.hamlet")
