0319e3d97e3631dd2bbf07c528bac9db926b7f71
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 18:55:09 +0000

    Teste P2

--- src/Handler/UserLogin.hs
+++ src/Handler/UserLogin.hs
@@ -28,6 +28,9 @@ getUserLoginR = do
         toWidgetHead [hamlet|
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         |]
+        $(whamletFile "templates/menu.hamlet")
+        $(whamletFile "templates/login.hamlet")
+        $(whamletFile "templates/footer.hamlet")
         [whamlet|
             $maybe mensa <- msg
                 <div>
