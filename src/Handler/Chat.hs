454620d776154b36f2dfb38fb75bee999bb89032
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 00:06:47 +0000

    Teste P2

--- src/Handler/Chat.hs
+++ src/Handler/Chat.hs
@@ -42,7 +42,7 @@ postChatR = do
         FormSuccess chat -> do 
             username <- lookupSession "_NOME"
             now <- liftIO getCurrentTime
-            runDB $ insert username chat now 
+            runDB $ insert username now chat 
             setMessage [shamlet|
                 <div>
                     MENSAGEM POSTADA
