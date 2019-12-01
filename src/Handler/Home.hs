ed6c2174955cedc304a418cbcbb5f10a62ad5c6d
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 01:20:19 +0000

    Teste P2

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -27,6 +27,7 @@ getPage1R = do
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
+    sess <- lookupSession "_NOME"
     --addStylesheet (StaticR css_bootstrap_css)
     --addScript (StaticR js_main_js)
         setTitle "Aula Haskell Fatec"
@@ -56,42 +57,49 @@ getHomeR = do
         [whamlet|
         
             <div class="container">
-            
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div class="collapse navbar-collapse">
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">
-                                    Home
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Jogo
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">
-                                        Descrição
-                                    <a class="dropdown-item" href="#">
-                                        Jogar
-                                        
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src=@{StaticR slide1_jpg}>
-                            <div class="carousel-caption d-none d-md-block">
-                                <span class="legenda">
-                                    Em breve...
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src=@{StaticR slide2_jpg}>
-                            <div class="carousel-caption d-none d-md-block">
-                                <span class="legenda">
-                                    Em breve...
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true">
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true">
-                    
-                    
-            <div class="foot">
-                <img id="imgfatec" src=@{StaticR fatec_png}>
-                <span class="copytext">
-                    Desenvolvido para a disciplina de Tópicos Especiais em Informática na Fatec BS
+            $   maybe nomeSess <- sess
+                    <div> 
+                        Ola #{nomeSess}  
+                        <div>
+                            teste 1
+                        <form action="@{LogoutR}" method=post>
+                            <input type="submit" value="Sair">
+                $nothing
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <div class="collapse navbar-collapse">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">
+                                        Home
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Jogo
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">
+                                            Descrição
+                                        <a class="dropdown-item" href="#">
+                                            Jogar
+                                            
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src=@{StaticR slide1_jpg}>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <span class="legenda">
+                                        Em breve...
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src=@{StaticR slide2_jpg}>
+                                <div class="carousel-caption d-none d-md-block">
+                                    <span class="legenda">
+                                        Em breve...
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true">
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true">
+                        
+                        
+                <div class="foot">
+                    <img id="imgfatec" src=@{StaticR fatec_png}>
+                    <span class="copytext">
+                        Desenvolvido para a disciplina de Tópicos Especiais em Informática na Fatec BS
         |]
