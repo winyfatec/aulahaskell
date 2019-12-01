d6d9e2d2e51a92e24239ec4f7d087268509156ed
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 00:26:14 +0000

    Teste P2

--- src/Handler/Chat.hs
+++ src/Handler/Chat.hs
@@ -15,8 +15,8 @@ import Data.Time
 import Control.Monad.IO.Class
 
 -- renderDivs
-formChat :: Form Text
-formChat = renderBootstrap $ Text
+formChat :: Form (Text)
+formChat = renderBootstrap $ ()
     <$> areq textField "Mensagem: " Nothing
 
 getChatR :: Handler Html
