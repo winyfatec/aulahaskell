fde0520be4cf77c46389ae4bcf93e6d0f0a70375
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 01:55:57 +0000

    Teste P2

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -55,7 +55,7 @@ getHomeR = do
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
         |]
         let tstvar = "teste" :: Text
-        $(whamletFile "templates/menu.hamlet")
+        --$(whamletFile "templates/menu.hamlet")
         [whamlet|
         
             <div class="container">
