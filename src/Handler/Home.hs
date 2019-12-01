95616d7b57da1debf0d17a02cabdc505d6c035e9
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-01 01:00:26 +0000

    Teste P2

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -43,7 +43,7 @@ getHomeR = do
         |]
         toWidgetHead [cassius|
             h1
-                color : blue;
+                color : black;
         |]    
         toWidgetHead [hamlet|
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
