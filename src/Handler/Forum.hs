1ef3807cee5fd8d0af63d5f1d42ed3bcc2b53124
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-07 18:57:17 +0000

    P2 Teste

--- src/Handler/Forum.hs
+++ src/Handler/Forum.hs
@@ -17,7 +17,7 @@ formForum = renderBootstrap $ Forum
 
 getForumR :: Handler Html
 getForumR = do
-    //threads <- runDB $ selectList [] [Asc ForumTiulo]
+    --threads <- runDB $ selectList [] [Asc ForumTiulo]
 --    (widget,enctype) <- generateFormPost formForum
     defaultLayout $ do
         msg <- getMessage
