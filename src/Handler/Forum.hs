26c8af64814fc3c04481a19f7345ee51fcfa2d55
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-07 22:22:14 +0000

    P2 Teste

--- src/Handler/Forum.hs
+++ src/Handler/Forum.hs
@@ -46,7 +46,7 @@ getForumR = do
 postForumR :: Handler Html
 postForumR = do
     cria <- lookupPostParam "titulo"
-    dt <- liftIO getCurrentTime
+    dt <- getCurrentTime
     username <- lookupSession "_NOME"
     case cria of
         Just titulo -> do
