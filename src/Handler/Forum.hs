a6d71242d0600de683dd2d3df6ccee6cc50b466c
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-08 17:13:14 +0000

    P2 Teste

--- src/Handler/Forum.hs
+++ src/Handler/Forum.hs
@@ -58,7 +58,7 @@ postForumR :: Handler Html
 postForumR = do
     cria <- lookupPostParam "titulo"
     Just userId <- lookupSession "_USUARIO"
-    --Just usuario <- runDB $ get404 (toSqlKey $ read userId :: Int64)
+    Just usuario <- runDB $ get404 (toSqlKey (read userId :: Int64))
     -- Just uid <- runDB $ get (UserId usuario)
     t <- read userId :: Int64
     criado <- (liftIO getCurrentTime)
@@ -66,7 +66,6 @@ postForumR = do
         Just titulo -> do
             --runDB $ insert $ Forum titulo (UserId usuario) criado
             setMessage [shamlet|
-                #{t}
                 Thread criada com sucesso!
             |]
             redirect ForumR
