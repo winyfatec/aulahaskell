10e403f4875873adbf91a2cd8f2111849ab3a19a
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-08 22:34:06 +0000

    P2 Jogos (Fix)

--- src/Handler/Jogo.hs
+++ src/Handler/Jogo.hs
@@ -30,9 +30,9 @@ getJogoR jid = do
         |]
         
         let jogos = case jid of
-            1 -> show $ StaticR jogos_atividadeCorredor_swf
-            2 -> show $ StaticR jogos_AtividadeShooter_swf
-            3 -> show $ StaticR jogos_JogoLutaV4_swf
+                1 -> show $ StaticR jogos_atividadeCorredor_swf
+                2 -> show $ StaticR jogos_AtividadeShooter_swf
+                3 -> show $ StaticR jogos_JogoLutaV4_swf
                         
         $(whamletFile "templates/menu.hamlet")
         $(whamletFile "templates/jogo.hamlet")
