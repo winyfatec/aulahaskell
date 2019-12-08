2711171f3929760deec6ea6e519e12ea8169b766
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-08 02:23:19 +0000

    P2 Teste

--- src/Handler/Thread.hs
+++ src/Handler/Thread.hs
@@ -19,7 +19,7 @@ formThread = renderBootstrap $ Discussao
     <*> aopt hiddenField "username" Nothing
 -}
 
-getThreadR :: id -> Handler Html
+getThreadR :: ID -> Handler Html
 getThreadR pid = do
     sess <- lookupSession "_NOME"
     thd <- runDB $ selectList [pid] []
