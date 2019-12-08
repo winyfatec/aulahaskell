57e551ab406a081bce37a60cef178ee2418bf24d
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-08 00:46:23 +0000

    P2 Teste

--- src/Handler/Index.hs
+++ src/Handler/Index.hs
@@ -15,29 +15,19 @@ getIndexR :: Handler Html
 getIndexR = do
     defaultLayout $ do
         sess <- lookupSession "_NOME"
-    --addStylesheet (StaticR css_bootstrap_css)
-    --addScript (StaticR js_main_js)
-        setTitle "Aula Haskell Fatec"
-        --addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        --addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
-        --addScriptRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        --addScriptRemote "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async
+         setTitle "Aula Haskell Fatec"
         addStylesheet $ StaticR css_main_css
         addScript $ StaticR js_main_js
-        toWidgetHead [julius|
-            function ola(){
-                alert("OI");
-            }
-        |]
-        $(whamletFile "templates/header.hamlet")
-        {-toWidgetHead [hamlet|
+        addScript $ StaticR js_pacman_js
+        toWidgetHead [hamlet|
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-3156965201812393",enable_page_level_ads: true});
             <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-        |]-}
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" integrity="sha256-0rguYS0qgS6L4qVzANq4kjxPLtvnp5nn2nB5G1lWRv4=" crossorigin="anonymous">
+        |]
         $(whamletFile "templates/menu.hamlet")
         $(whamletFile "templates/index.hamlet")
         $(whamletFile "templates/footer.hamlet")
