cca1150304a1e4e809740b89ad2ed7d4dda25b5c
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-08 15:17:17 +0000

    P2 Teste

--- src/Handler/Forum.hs
+++ src/Handler/Forum.hs
@@ -61,7 +61,7 @@ postForumR = do
     criado <- (liftIO getCurrentTime)
     case cria of
         Just titulo -> do
-            runDB $ insert $ Forum titulo usuario criado
+            runDB $ insert $ Forum titulo (Just usuario) criado
             setMessage [shamlet|
                 Thread criada com sucesso!
             |]
