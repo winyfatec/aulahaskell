214fe7f509fdc2fdff4b2a74f15d60a4eaaae776
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-08 00:06:02 +0000

    P2 Teste

--- src/Handler/Index.hs
+++ src/Handler/Index.hs
@@ -29,7 +29,7 @@ getIndexR = do
                 alert("OI");
             }
         |]
-        toWidgetHead $(whamletFile "templates/header.hamlet")
+        toWidgetHead whamletFile "templates/header.hamlet"
         toWidgetHead [hamlet|
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
