0ddcebf8c94de7e053375dcafe938f5a0fcc0dbf
Ubuntu <ubuntu@ip-172-31-95-254.ec2.internal>
2019-12-10 01:03:14 +0000

    P2 (Fix)

--- src/Handler/Registration.hs
+++ src/Handler/Registration.hs
@@ -22,24 +22,19 @@ getRegisterR = do
     (widget,enctype) <- generateFormPost formRegister
     defaultLayout $ do
         msg <- getMessage
+        setTitle "Aula Haskell Fatec :: Login"
+        sess <- lookupSession "_NOME"
+        addStylesheet $ StaticR css_main_css
+        addScript $ StaticR js_main_js
         toWidgetHead [hamlet|
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
             <link rel="shortcut icon" href="@{StaticR icons_fontawesome_solid_crosshairs_svg}" type="image/x-icon" />
         |]
-        [whamlet|
-            $maybe mensa <- msg
-                <div .alert.alert-success>
-                    ^{mensa}
-            $nothing
-            
-            <h1>
-                Cadastro de Usuario
-            <form method=post action=@{RegisterR}>
-                ^{widget}
-                <input .btn.btn-primary type="submit" value="Registrar">
-        |]
+
+        $(whamletFile "templates/register.hamlet")
+        $(whamletFile "templates/footer.hamlet")
 
 
 postRegisterR :: Handler Html
